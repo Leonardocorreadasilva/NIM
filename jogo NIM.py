292ce3fe12bc6b781d745e55c1bc05b73cdbe4ce
Leonardo <97353984+Leonardocorreadasilva@users.noreply.github.com>
2022-05-12 20:36:14 -0300

    Primeiro envio

---
+++ jogo NIM.py
@@ -0,0 +1,134 @@
+import os
+def computador_escolhe_jogada(n, m):
+    computadorRemove = 1
+
+    while computadorRemove != m:
+        if (n - computadorRemove) % (m+1) == 0:
+            return computadorRemove
+
+        else:
+            computadorRemove += 1
+
+    return computadorRemove
+
+
+def usuario_escolhe_jogada(n, m):
+    jogadaValida = False
+
+    while not jogadaValida:
+        jogadorRemove = int(input('Quantas peças você vai tirar? '))
+        if jogadorRemove > m or jogadorRemove < 1:
+            print()
+            print('Oops! Jogada inválida! Tente de novo.')
+            print()
+
+        else:
+            jogadaValida = True
+
+    return jogadorRemove
+
+
+def campeonato():
+    numeroRodada = 1
+    while numeroRodada <= 3:
+        print()
+        print('**** Rodada', numeroRodada, '****')
+        print()
+        partida()
+        numeroRodada += 1
+    print()
+    print('Placar: Você 0 X 3 Computador')
+
+
+def partida():
+    n = int(input('Quantas peças? '))
+
+    m = int(input('Limite de peças por jogada? '))
+
+    vezDoPC = False
+
+    if n % (m+1) == 0:
+        print()
+        print('Voce começa!')
+
+    else:
+        print()
+        print('Computador começa!')
+        vezDoPC = True
+
+    while n > 0:
+        if vezDoPC:
+            computadorRemove = computador_escolhe_jogada(n, m)
+            n = n - computadorRemove
+            if computadorRemove == 1:
+                print()
+                print('O computador tirou uma peça')
+            else:
+                print()
+                print('O computador tirou', computadorRemove, 'peças')
+
+            vezDoPC = False
+        else:
+            jogadorRemove = usuario_escolhe_jogada(n, m)
+            n = n - jogadorRemove
+            if jogadorRemove == 1:
+                print()
+                print(f'Você tirou {jogadorRemove} peça')
+            else:
+                print()
+                print('Você tirou', jogadorRemove, 'peças')
+            vezDoPC = True
+        if n == 1:
+            print('Agora resta apenas uma peça no tabuleiro.')
+            print()
+        else:
+            if n != 0:
+                print('Agora restam,', n, 'peças no tabuleiro.')
+                print()
+
+    print('Fim do jogo! O computador ganhou!')
+
+print('Nim é um jogo matemático onde você escolhe um numero de peças para serem retiradas e você escolhe o limite de peças para serem retiradas.')
+
+
+def menu():
+    os.system('cls')
+    print('Bem-vindo ao jogo do NIM! Escolha:')
+    print()
+
+    print('1 - para jogar uma partida isolada: ')
+    tipoDePartida = input('2 - para jogar um campeonato \n Digitar 0 fecha na hora de escolher o modo de jogo fecha o programa:\nEscolha: ')
+
+    if tipoDePartida == '2':
+        print()
+        print('Voce escolheu um campeonato!')
+        print()
+        campeonato()
+    elif tipoDePartida == '1':
+        print()
+        partida()
+
+    elif tipoDePartida == '0':
+        exit()
+    else:
+        print('invalido')
+        menu()
+menu()
+'''
+else:
+    while tipoDePartida != 1 or tipoDePartida != 2:
+        print('Você digitou um numero inválido!!!')
+        tipoDePartida = int(input('Digite somente 1 para partida unica \n 2 - para jogar um campeonato:'))
+        
+        if tipoDePartida == 2:
+            print()
+            print('Voce escolheu um campeonato!')
+            print()
+            campeonato()
+        elif tipoDePartida == 1:
+            print('Você escolheu a partida única: \nEscolha: ')
+            partida()
+        elif tipoDePartida == 0:
+            exit()
+'''
+        
